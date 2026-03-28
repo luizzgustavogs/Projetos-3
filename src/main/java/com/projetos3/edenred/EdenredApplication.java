1827fae152ca08a0ede348ca9a6d654872ed356d
Eduardo <e.h.alves.santos@gmail.com>
2026-03-28 09:21:59 -0300

    Estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/projetos3/edenred/EdenredApplication.java
@@ -0,0 +1,13 @@
+package com.projetos3.edenred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdenredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdenredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetos3/edenred/EdenredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos3.edenred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdenredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
